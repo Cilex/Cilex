0cbd969187980406a51909df6d519086a0233f7d
Ben Selby <benmatselby@gmail.com>
2012-04-14 21:48:39 +0100

    Add the ability to have a json configuration file

--- src/Cilex/Provider/ConfigServiceProvider.php
+++ src/Cilex/Provider/ConfigServiceProvider.php
@@ -39,6 +39,9 @@ class ConfigServiceProvider implements ServiceProviderInterface
                 case 'xml':
                     $result = simplexml_load_file($app['config.path']);
                     break;
+                case 'json':
+                    $result = json_decode(file_get_contents($app['config.path']));
+                    break;
                 default:
                     throw new \InvalidArgumentException(
                         'Unable to load configuration; the provided file '
